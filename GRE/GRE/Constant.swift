ecb297e5fa71f1819b89656f6f734feb2ace7fb8
nguyensonvu1102 <mrnguyensonvuads@gmail.com>
2016-07-11 11:45:26 +0700

    ........

--- GRE/GRE/Constant.swift
+++ GRE/GRE/Constant.swift
@@ -6,7 +6,15 @@
 //  Copyright © 2016 Mr.Vu. All rights reserved.
 //
 
+import UIKit
+
 let NEW_WORD_TAG = "new word"
 let LEARNING_TAG = "learning"
 let REVIEW_TAG = "review"
 let MASTER_TAG = "master"
+
+// Color
+let MASTER_TAG_COLOR      = UIColor(netHex: 0x8bc34a)
+let LEARNING_TAG_COLOR    = UIColor(netHex: 0xe74c3c)
+let REVIEW_TAG_COLOR      = UIColor(netHex: 0x00bcd4)
+let NEW_WORD_TAG_COLOR    = UIColor(netHex: 0x607d8b)
--- GRE/GRE/FrontFlashCardViewModel.swift
+++ GRE/GRE/FrontFlashCardViewModel.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class FrontFlashCardViewModel: UIView {
     
+    @IBOutlet weak var vTag: UIView!
     @IBOutlet weak var lblWord: UILabel!
     @IBOutlet weak var lblTag: UILabel!
     var card : Card! {
@@ -19,12 +20,29 @@ class FrontFlashCardViewModel: UIView {
     }
     
     override func awakeFromNib() {
-
+        self.layer.shadowColor = UIColor.grayColor().CGColor
+        self.layer.shadowOpacity = 0.8
+        self.layer.shadowOffset = CGSizeZero
+        self.layer.shadowRadius = 5
+        self.layer.cornerRadius = 13
     }
     
     func layout() {
         self.lblWord.text = card.word
-        self.lblTag.text = card.tag
+        self.lblTag.text = card.tag.uppercaseString
+        
+        if card.tag == NEW_WORD_TAG {
+            self.vTag.backgroundColor = NEW_WORD_TAG_COLOR
+        }
+        else if card.tag == LEARNING_TAG {
+            self.vTag.backgroundColor = LEARNING_TAG_COLOR
+        }
+        else if card.tag == REVIEW_TAG {
+            self.vTag.backgroundColor = REVIEW_TAG_COLOR
+        }
+        else if card.tag == MASTER_TAG {
+            self.vTag.backgroundColor = MASTER_TAG_COLOR
+        }
     }
     
     required init?(coder aDecoder: NSCoder) {
